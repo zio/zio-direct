e6efd5d2e9589bfcc4a52464ed24e19038066e66
Milad Khajavi <khajavi@gmail.com>
2022-12-23 21:57:57 +0330

    move readme to index page. (#25)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,12 @@
 const sidebars = {
   sidebar: [
-    "index"
+    {
+      type: "category",
+      label: "ZIO Direct Style",
+      collapsed: false,
+      link: { type: "doc", id: "index" },
+      items: [ ]
+    }
   ]
 };
 
