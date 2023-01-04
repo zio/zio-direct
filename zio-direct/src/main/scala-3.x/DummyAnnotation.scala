ee46bf2d64596fa1535a064d92e62f8954ea6dc9
Alexander Ioffe <deusaquilus@gmail.com>
2023-01-03 23:36:50 -0500

    Attempt to fix useless post-typer error

--- zio-direct/src/main/scala-3.x/DummyAnnotation.scala
+++ zio-direct/src/main/scala-3.x/DummyAnnotation.scala
@@ -1,4 +1,4 @@
 package scala.reflect.macros.internal
 
 // A dummy annotation for IntelliJ to recognize a Scala 3 inline macro... as a macro
-final class macroImpl extends scala.annotation.StaticAnnotation
+final class macroImpl(referenceToMacroImpl: Any) extends scala.annotation.StaticAnnotation
