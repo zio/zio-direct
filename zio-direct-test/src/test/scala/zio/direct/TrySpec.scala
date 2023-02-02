b6e23bc3350a9d8aa881af77704ed00a63f32b3b
Alexander Ioffe <deusaquilus@gmail.com>
2023-02-02 00:41:20 -0500

    Fixing up Scala 2 stuff

--- zio-direct-test/src/test/scala/zio/direct/TrySpec.scala
+++ zio-direct-test/src/test/scala/zio/direct/TrySpec.scala
@@ -87,28 +87,6 @@ object TrySpec extends DeferRunSpec {
           assertIsType[ZIO[Any, Exception, Int]](out) andAssert
             assertZIO(out)(equalTo(1))
         },
-        test("not caught error in parallel block - error should be fail-channel") {
-          val out = defer {
-            try {
-              (123, { throw new FooError })
-            } catch {
-              case `e` => 111
-            }
-          }
-          assertIsType[ZIO[Any, FooError, (Int, Nothing) | 111]](out) andAssert
-            assertZIO(out.exit)(fails(isSubtype[FooError](anything)))
-        },
-        test("not caught error in parallel block (fail-channel)") {
-          val out = defer {
-            try {
-              (123, attempt(throw new FooError).run)
-            } catch {
-              case `e` => 111
-            }
-          }
-          assertIsType[ZIO[Any, Throwable, (Int, Nothing) | 111]](out) andAssert
-            assertZIO(out.exit)(fails(isSubtype[FooError](anything)))
-        },
         test("not caught error in parallel block (die-channel) - one side") {
           // even thought we create the exception inside a success block, the actual
           // 'throw' term is being used so a ZIO.fail(error) is called. therefore
--- zio-direct/src/main/scala-2.x/zio/direct/core/util/WithInterpolator.scala
+++ zio-direct/src/main/scala-2.x/zio/direct/core/util/WithInterpolator.scala
@@ -1,13 +1,11 @@
 package zio.direct.core.util
 
 import zio.direct.core.metaprog.WithIR
-import zio.direct.core.metaprog.WithF
 import zio.direct.core.metaprog.WithPrintIR
 import zio.direct.core.metaprog.WithZioType
-import zio.direct.core.norm.WithComputeType
 
 trait WithInterpolator extends WithInterpolatorBase {
-  self: WithF with WithIR with WithZioType with WithPrintIR =>
+  self: WithIR with WithZioType with WithPrintIR =>
 
   override def printAny(any: Any): String = PrintAny(any)
 }
