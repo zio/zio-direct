447483623d18b734e545b1569b44d9dac7d8a928
Alexander Ioffe <deusaquilus@gmail.com>
2023-02-02 00:05:13 -0500

    Remove invalid cross-compile dependency

---
+++ zio-direct/src/main/scala-3.x/zio/direct/core/util/F3Util.scala
@@ -0,0 +1,11 @@
+package zio.direct.core.util
+
+import zio.direct.MonadFallible
+
+object F3Util {
+  def wrapWithThrowable[F[_, _, _], R, E, A](input: F[R, E, A])(implicit monad: MonadFallible[F]): F[R, Throwable, A] =
+    monad.mapError(input) {
+      case t: Throwable => t
+      case other        => new RuntimeException(s"Effect failed due to: $other")
+    }
+}
--- zio-direct/src/main/scala/zio/direct/core/util/ZioUtil.scala
+++ zio-direct/src/main/scala/zio/direct/core/util/ZioUtil.scala
@@ -1,10 +1,10 @@
 package zio.direct.core.util
 
-import zio.direct.MonadFallible
+import zio.ZIO
 
-object F3Util {
-  def wrapWithThrowable[F[_, _, _], R, E, A](input: F[R, E, A])(implicit monad: MonadFallible[F]): F[R, Throwable, A] =
-    monad.mapError(input) {
+object ZioUtil {
+  def wrapWithThrowable[R, E, A](zio: ZIO[R, E, A]) =
+    zio.mapError {
       case t: Throwable => t
       case other        => new RuntimeException(s"ZIO failed due to: $other")
     }
