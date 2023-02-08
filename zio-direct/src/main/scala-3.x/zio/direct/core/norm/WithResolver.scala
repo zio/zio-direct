8293e475b2860353dd41a537e5966e129d5ac3bf
Alexander Ioffe <deusaquilus@gmail.com>
2023-02-08 03:21:35 -0500

    Bring back asExprOfOrFail? Not sure

--- zio-direct/src/main/scala-3.x/zio/direct/core/norm/WithResolver.scala
+++ zio-direct/src/main/scala-3.x/zio/direct/core/norm/WithResolver.scala
@@ -35,26 +35,26 @@ trait WithResolver {
     val inf = Inferred(anyToNothing)
   }
 
-  // extension (expr: Expr[_])
-  //   def asExprOfOrFail[T: Type]: Expr[T] =
-  //     Type.of[T] match
-  //       case '[t] =>
-  //         if (!(expr.asTerm.tpe <:< TypeRepr.of[t]))
-  //           report.errorAndAbort(
-  //             s"The type of the expression `${Format.Expr(expr)}`\n===== Was: ==================\n${Format.TypeRepr(expr.asTerm.tpe)}\n===== But Expected: =========\n${Format.TypeRepr(TypeRepr.of[t])}\n-----------------------------"
-  //           )
-  //         else
-  //           expr.asExprOf[T]
-  // extension (term: Term)
-  //   def asExprOfOrFail[T: Type]: Expr[T] =
-  //     Type.of[T] match
-  //       case '[t] =>
-  //         if (!(term.tpe <:< TypeRepr.of[t]))
-  //           report.errorAndAbort(
-  //             s"The type of the expression `${Format.Term(term)}`\n===== Was: ==================\n${Format.TypeRepr(term.tpe)}\n===== But Expected: =========\n${Format.TypeRepr(TypeRepr.of[t])}\n-----------------------------"
-  //           )
-  //         else
-  //           term.asExprOf[T]
+  extension (expr: Expr[_])
+    def asExprOfOrFail[T: Type]: Expr[T] =
+      Type.of[T] match
+        case '[t] =>
+          if (!(expr.asTerm.tpe <:< TypeRepr.of[t]))
+            report.errorAndAbort(
+              s"The type of the expression `${Format.Expr(expr)}`\n===== Was: ==================\n${Format.TypeRepr(expr.asTerm.tpe)}\n===== But Expected: =========\n${Format.TypeRepr(TypeRepr.of[t])}\n-----------------------------"
+            )
+          else
+            expr.asExprOf[T]
+  extension (term: Term)
+    def asExprOfOrFail[T: Type]: Expr[T] =
+      Type.of[T] match
+        case '[t] =>
+          if (!(term.tpe <:< TypeRepr.of[t]))
+            report.errorAndAbort(
+              s"The type of the expression `${Format.Term(term)}`\n===== Was: ==================\n${Format.TypeRepr(term.tpe)}\n===== But Expected: =========\n${Format.TypeRepr(TypeRepr.of[t])}\n-----------------------------"
+            )
+          else
+            term.asExprOf[T]
 
   // Right now typing this at WithReconstructTree but really will need to get it from input signatures
   class Resolver[F[_, _, _]: Type, S: Type, W: Type](zpe: ZioType, directMonad: DirectMonad[F, S, W]) {
@@ -66,7 +66,7 @@ trait WithResolver {
       (monad.zpe.asTypeTuple, zpe.valueType) match
         case (('[r], '[e], '[a]), '[b]) =>
           '{
-            $MonadSuccess.flatMap[r, e, a, b](${ monad.term.asExpr /* .asExprOf[F[r, e, a]] */ }.asInstanceOf[F[r, e, a]])( // .asInstanceOf[F[r, e, a]]
+            $MonadSuccess.flatMap[r, e, a, b](${ monad.term.asExprOfOrFail[F[r, e, a]] })( // .asInstanceOf[F[r, e, a]]
               ${ applyLambda.term.asExpr }.asInstanceOf[a => F[r, e, b]])
           }.toZioValue(zpe)
 
@@ -84,7 +84,7 @@ trait WithResolver {
       (monad.zpe.asTypeTuple, zpe.asTypeTuple) match
         case (('[r], '[e], '[a]), ('[or], '[oe], '[b])) =>
           val out = '{
-            $MonadSuccess.map[r, e, a, b](${ monad.term.asExprOf[F[r, e, a]] })( // .asInstanceOf[F[r, e, a]]
+            $MonadSuccess.map[r, e, a, b](${ monad.term.asExprOfOrFail[F[r, e, a]] })( // .asInstanceOf[F[r, e, a]]
               ${ applyLambdaTerm.asExpr }.asInstanceOf[a => b])
           }
           out.toZioValue(zpe)
